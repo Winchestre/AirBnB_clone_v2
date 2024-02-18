9c222a2c9c8be506e522267f9862be613e9a93d8
Ndman90 <onwuamaegbund99@gmail.com>
2024-02-18 13:40:31 -0800

    Number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -46,5 +46,6 @@ def number_template(n):
     if isinstance(n, int):
         return render_template("5-number.html", n=n)
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=None)
